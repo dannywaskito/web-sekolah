660596a5f7bf14900b32e706d2710072796e5ccb
Muhammad Danny Waskito <dannywaskito64@gmail.com>
2021-04-25 22:55:49 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,22 @@
-$('.filter-button').on('click', (e) => {
-  const filter = $(e.target).attr('data-filter');
-  console.log(filter);
- 	const items = $('.item-content').parent();
-  for (item of items) {
-    if (filter == '') {
-      
-        $(item).addClass('animated zoomIn faster');
-      $(item).parent().addClass('animated zoomIn faster');
-      $(item).removeClass('d-none');
-      $(item).parent().removeClass('d-none');
-      
-     
-      console.log('x');
-    }else if ($(item).attr('data-category') == filter) {
-      $(item).addClass('animated zoomIn faster');
-      $(item).parent().addClass('animated zoomIn faster');
-      $(item).removeClass('d-none');
-      $(item).parent().removeClass('d-none');
-    } else {
-      $(item).addClass('d-none');
-      $(item).parent().addClass('d-none');
-      $(item).removeClass('animated zoomIn faster');
-      $(item).parent().removeClass('animated zoomIn faster');
-    }
-  }
-});
+ $('.pendidikan-menu ul li').click(function(){
+          $('.pendidikan-menu ul li').removeClass('active');
+          $(this).addClass('active');
+          
+          var selector = $(this).attr('data-filter');
+          $('.pendidikan-item').isotope({
+            filter:selector
+          });
+          return  false;
+         });
+         $(document).ready(function() {
+         var popup_btn = $('.popup-btn');
+         popup_btn.magnificPopup({
+         type : 'image',
+         gallery : {
+          enabled : true
+         }
+         });
+         });
   var typed = new Typed(".typing", {
             strings: ["Web Developer", "Web Designer"],
             typeSpeed: 100,
